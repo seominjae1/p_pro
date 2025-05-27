9318da46f8bfaef02f6b75bb601e7e132251b9dd
seominjae1 <153708875+seominjae1@users.noreply.github.com>
2025-05-27 09:48:39 +0900

    Add files via upload

---
+++ calculator/src/main/java/gcukakao/jenkins/calculator/CalculatorController.java
@@ -0,0 +1,20 @@
+package gcukakao.jenkins.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class CalculatorController {
+    @Autowired
+    private Calculator calculator;
+
+    @RequestMapping("/sum")
+    String sum(@RequestParam("a") Integer a,@RequestParam("b") Integer b){
+        return String.valueOf(calculator.sum(a,b));
+    }
+
+    
+}
